dd30134d394b92caadffe52b4a3c6993f75e622c
shengk <1006533962@qq.com>
2020-11-06 23:02:27 +0800

    注释，第一次提交

---
+++ module0001/src/Person.java
@@ -0,0 +1,12 @@
+/**
+ * @Auther: kangsheng
+ * @Date: 2020/11/6 - 11 - 06 - 23:01
+ * @Description: PACKAGE_NAME
+ * @version: 1.0
+ */
+public class Person {
+    //这是一个main方法，是程序的入口：
+    public static void main(String[] args) {
+
+    }
+}
