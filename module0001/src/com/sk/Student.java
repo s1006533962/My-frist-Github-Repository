6d576f2c94c776f396ea69101063cc9bb7d1ff1b
shengk <1006533962@qq.com>
2020-11-06 22:12:00 +0800

    注释，第一次提交

--- module0001/src/com/sk/Student.java
+++ module0001/src/com/sk/Student.java
@@ -1,5 +1,7 @@
 package com.sk;
 
+import java.security.PrivateKey;
+
 /**
  * @Auther: kangsheng
  * @Date: 2020/11/6 - 11 - 06 - 21:59
@@ -7,4 +9,8 @@ package com.sk;
  * @version: 1.0
  */
 public class Student {
+    //这是一个main方法，是程序的入口：
+    public static void main(String[] args){
+
+    }
 }
